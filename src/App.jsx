fce0027638b91e1a7168f44211a9f78c20c65c22
Emanuel Cortese <emanuelcortese_95@hotmail.com>
2022-04-05 21:02:56 -0300

    Extracted logic from App component and created getPokemons service

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,31 @@
 import { useState, useEffect } from 'react'
-import { BASE_URL } from '../config'
+import getPokemons from './services/getPokemons'
 import './App.css'
-
-function getTypes (types) {
-  return types?.map(el => el.type.name)
-}
-
-function getUrlImage (obj) {
-  return obj?.other?.dream_world?.front_default
-}
 
 function App () {
   const [data, setData] = useState([])
 
   useEffect(() => {
-    ;(async () => {
-      // obtener url de cada pokemon
-      const res = await window.fetch(`${BASE_URL}/pokemon?limit=10&offset=0`)
-      const { results } = await res.json()
-
-      // crear array de promesas con las urls obtenidas
-      const promises = results.map(({ url }) => window.fetch(url).then(res => res.json()))
-      // Promise.allSettled resuelve el array de promesas
-      const resPromiseAll = await Promise.allSettled(promises)
-
-      // extraer datos necesarios para mostrar
-      const dataToShow = resPromiseAll.map(res => res.value)
-
-      setData(dataToShow)
-    })()
-  }, [BASE_URL])
+    getPokemons().then(data => { setData(data) })
+  }, [])
 
   return (
     <div className='App'>
       <h1>PokeApp</h1>
       {
-        data.map(({ id, name, types, sprites }) => {
-          const typesList = getTypes(types)
-          const urlImage = getUrlImage(sprites)
-
+        data?.map(({ id, name, PokemonTypes, PokemonUrlImage }) => {
           return (
             <div key={id}>
               <h2>{name}</h2>
-              <img loading='lazy' src={urlImage} alt={name} />
+              <img loading='lazy' src={PokemonUrlImage} alt={name} />
               <div>
                 {
-                  typesList?.map(type => <span key={type}>{type}</span>)
+                  PokemonTypes?.map(type => <span key={type}>{type}</span>)
                 }
               </div>
             </div>
           )
         })
-
       }
     </div>
   )
---
+++ src/services/getPokemons.js
@@ -0,0 +1,33 @@
+import { BASE_URL } from '../../config'
+
+const getTypes = ({ types }) => {
+  return types?.map(el => el.type.name)
+}
+
+const getUrlImage = ({ sprites }) => {
+  return sprites?.other?.dream_world?.front_default
+}
+
+const ApiResponse = ({ dataToShow } = {}) => {
+  return dataToShow.map(({ id, name, types, sprites }) => {
+    const PokemonTypes = getTypes({ types })
+    const PokemonUrlImage = getUrlImage({ sprites })
+    return { id, name, PokemonTypes, PokemonUrlImage }
+  })
+}
+
+export default async function getPokemons () {
+  // obtener url de cada pokemon
+  const res = await window.fetch(`${BASE_URL}/pokemon?limit=10&offset=0`)
+  const { results } = await res.json()
+
+  // crear array de promesas con las urls obtenidas
+  const promises = results.map(({ url }) => window.fetch(url).then(res => res.json()))
+  // Promise.allSettled resuelve el array de promesas
+  const resPromiseAll = await Promise.allSettled(promises)
+
+  // extraer datos necesarios para mostrar
+  const dataToShow = resPromiseAll.map(res => res.value)
+
+  return ApiResponse({ dataToShow })
+}
