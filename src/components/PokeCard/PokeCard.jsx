95e544e48424912c79527423656bce671ef0548b
Emanuel Cortese <emanuelcortese_95@hotmail.com>
2022-04-09 01:05:06 -0300

    Created PokeCard Component and created PokeCard styles with styled-components

---
+++ src/components/PokeCard/PokeCard.jsx
@@ -0,0 +1,15 @@
+import { CardContainer, CardID, CardImage, CardType, CardTypeContainer, CardTitle } from './style'
+export const PokeCard = ({ id, name, type, image }) => {
+  return (
+    <CardContainer>
+      <CardID>#{id}</CardID>
+      <CardImage loading='lazy' src={image} alt={name} />
+      <CardTitle>{name}</CardTitle>
+      <CardTypeContainer>
+        {
+            type?.map(type => <CardType PokeType={type} key={type}>{type}</CardType>)
+        }
+      </CardTypeContainer>
+    </CardContainer>
+  )
+}
---
+++ src/components/PokeCard/style.js
@@ -0,0 +1,106 @@
+import styled from 'styled-components'
+
+const backgroundType = ({ PokeType }) => {
+  switch (PokeType) {
+    case 'steel':
+      return '#CCCCCC'
+    case 'water':
+      return '#B0E2FF'
+    case 'bug':
+      return '#99CC33'
+    case 'dragon':
+      return '#AB82FF'
+    case 'electric':
+      return '#FFD700'
+    case 'ghost':
+      return '#778899'
+    case 'fire':
+      return '#FF7F00'
+    case 'fairy':
+      return '#FFB0FF'
+    case 'ice':
+      return '#ADD8E6'
+    case 'fighting':
+      return '#FF6A6A'
+    case 'normal':
+      return '#DDCCAA'
+    case 'grass':
+      return '#99FF66'
+    case 'psychic':
+      return '#FFB5C5'
+    case 'rock':
+      return '#CD853F'
+    case 'dark':
+      return '#A9A9A9'
+    case 'ground':
+      return '#DEB887'
+    case 'poison':
+      return '#CC88BB'
+    case 'flying':
+      return '#BAAAFF'
+  }
+}
+
+export const CardContainer = styled.div`
+    width: 20rem;
+    border: .063rem solid #000000;
+    border-radius: 1.3rem;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+    background:  radial-gradient(circle, var(--first-backgroundColor-card) , var(--second-backgroundColor-card)); ;
+    color: var(--color-card-primary);
+    box-shadow: 0 0 1rem var(--color-primary);
+`
+export const CardID = styled.span`
+    position: absolute;
+    color: var(--color-card-primary);
+    right: 0;
+    top: 0;
+    margin: 1rem 1rem 0 0;
+    font-weight: bold;
+    font-size: 1.4rem;
+    -webkit-text-stroke: .07rem #000000;
+`
+
+export const CardImage = styled.img`
+    width: 8rem;
+    height: 8rem;
+    transform: translateY(-4.5rem)
+`
+
+export const CardTitle = styled.h2`
+    position: absolute;
+    margin: 0;
+    font-weight: bold;
+    font-size: 2rem;
+    -webkit-text-stroke: .09rem #000000;
+    :first-letter{
+        text-transform: uppercase;
+    }
+`
+
+export const CardTypeContainer = styled.div`
+    margin-bottom: .8rem;
+`
+
+export const CardType = styled.span`
+    display: inline-block;
+    padding: .18rem 1.5rem;
+    margin: .6rem;
+    font-size: 1.1rem;
+    font-weight: 600;
+    border: 0.065rem solid var(--color-primary);
+    box-shadow: 0 0 .9rem var(--color-primary);
+    border-radius: 2rem;
+    text-aling: center;
+    color: var(--color-primary);
+    background: ${({ PokeType }) => {
+       return backgroundType({ PokeType })
+    }};
+    :first-letter{
+        text-transform: uppercase;
+    }
+`
