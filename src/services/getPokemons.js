085b795d355a9fb2d7a0c8c9d2272dd54110e77a
Emanuel Cortese <emanuelcortese_95@hotmail.com>
2022-06-30 18:51:42 -0300

    Feat and Delete: Add error handling and remove comments

--- src/services/getPokemons.js
+++ src/services/getPokemons.js
@@ -17,16 +17,16 @@ const ApiResponse = ({ dataToShow } = {}) => {
 }
 
 export default async function getPokemons ({ limit = 20, page = 0 } = {}) {
-  // obtener url de cada pokemon
   const res = await window.fetch(`${BASE_URL}/pokemon?limit=${limit}&offset=${page * limit}`)
+
+  if (res.status !== 200) {
+    const { url, status } = res
+    throw Error(`Error: ${status} in fetch ${url}`)
+  }
+
   const { results } = await res.json()
-
-  // crear array de promesas con las urls obtenidas
   const promises = results.map(({ url }) => window.fetch(url).then(res => res.json()))
-  // Promise.allSettled resuelve el array de promesas
   const resPromiseAll = await Promise.allSettled(promises)
-
-  // extraer datos necesarios para mostrar
   const dataToShow = resPromiseAll.map(res => res.value)
 
   return ApiResponse({ dataToShow })
