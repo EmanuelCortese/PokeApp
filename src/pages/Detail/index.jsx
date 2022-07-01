40a6e52c224d254655a653f433a564e983d0666c
Emanuel Cortese <emanuelcortese_95@hotmail.com>
2022-06-30 22:07:40 -0300

    Feat and Refactor: Add error handling and PokeHeader component

--- src/pages/Detail/index.jsx
+++ src/pages/Detail/index.jsx
@@ -2,36 +2,38 @@ import { useParams } from 'react-router-dom'
 import { useSinglePokemon } from '../../hooks/useSinglePokemon'
 import { PokeDetail } from '../../components/PokeDetail/PokeDetail'
 import { Spinner } from '../../components/Spinner/Spinner'
+import ErrorPage from '../Error404'
+import { PokeHeader } from '../../components/PokeHeader/PokeHeader'
 
 export default function Detail () {
   const { id } = useParams()
-  const { data, isLoading } = useSinglePokemon({ id })
+  const { data, isLoading, error } = useSinglePokemon({ id })
+
+  if (error) return <ErrorPage message='Sorry! But there was an error loading the Pokemon detail.' />
+
+  if (isLoading) return <Spinner />
 
   return (
-    <> {
-      isLoading
-        ? <Spinner />
-        : (
-          <>
-            {data.map(({ id, name, height, weight, PokemonTypes, PokemonUrlImage, PokemonSpecies, PokemonStats }) => {
-              return (
-                <div key={id}>
-                  <PokeDetail
-                    id={id}
-                    name={name}
-                    height={height}
-                    weight={weight}
-                    types={PokemonTypes}
-                    image={PokemonUrlImage}
-                    specie={PokemonSpecies}
-                    stats={PokemonStats}
-                  />
-                </div>
-              )
-            })}
-          </>
+    <>
+      <PokeHeader title='Detail' />
+      <>
+        {data.map(({ id, name, height, weight, PokemonTypes, PokemonUrlImage, PokemonSpecies, PokemonStats }) => {
+          return (
+            <div key={id}>
+              <PokeDetail
+                id={id}
+                name={name}
+                height={height}
+                weight={weight}
+                types={PokemonTypes}
+                image={PokemonUrlImage}
+                specie={PokemonSpecies}
+                stats={PokemonStats}
+              />
+            </div>
           )
-    }
+        })}
+      </>
 
     </>
   )
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,17 +2,17 @@ import { usePokemons } from '../../hooks/usePokemons'
 
 import { Spinner } from '../../components/Spinner/Spinner'
 import PokeList from '../../components/PokeList/PokeList'
+import ErrorPage from '../Error404'
+import { PokeHeader } from '../../components/PokeHeader/PokeHeader'
 
 export default function Home () {
-  const { loading } = usePokemons({ once: false })
+  const { loading, error } = usePokemons({ once: false })
+  if (error) return <ErrorPage message='Sorry! But an error occurred on the server when loading the Pokemon list.' />
+  if (loading) return <Spinner />
   return (
     <>
-      {
-        loading
-          ? <Spinner />
-          : <PokeList />
-      }
-
+      <PokeHeader show title='Pokedex' margin />
+      <PokeList />
     </>
   )
 }
