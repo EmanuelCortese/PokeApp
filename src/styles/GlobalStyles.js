392c3378fc4f51c741892b1f32d2743ae798e337
Emanuel Cortese <emanuelcortese_95@hotmail.com>
2022-09-21 22:52:52 -0300

    feat: add new styles for dark/light mode

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -2,29 +2,39 @@ import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyles = createGlobalStyle`
     :root{
-        --bacKground-primary:  #ECF0F1;
-        --color-primary: #18171C;
-        --first-backgroundColor-card: #8E44AD;
-        --second-backgroundColor-card: #4A235A;
-        --color-card-primary: #F4F4F4;
         --color-blue: #3d7dca;
         --color-yellow: #FFCB05;
         --speed-normal: 0.8s;
         --speed-fast: 0.4s;
+        --switch-sun-color: #FDFD96;
+        --switch-moon-color: #ADD8E6;
+    }
+
+    #dark{
+      --bacKground-primary:  #18171C;
+      --background-secondary: #F0EFF6;
+      --color-primary: #F4F4F4;
+      --color-secondary: #18171C;
+      --translateX-button: 1.4rem;
+      --switch-background: #ADD8E6;
+      }
+
+    #light{
+      --bacKground-primary: #F0EFF6;
+      --background-secondary: #18171C;
+      --color-primary: #18171C;
+      --color-secondary: #F4F4F4;
+      --translateX-button: -1.4rem;
+      --switch-background: #FDFD96;
     }
 
     body {
-        background-color: var(--bacKground-primary);
+        background-color: var(--bacKground-primary,  #18171C);
         color: var(--color-primary);
         margin: 0;
-        font-family: Helvetica, Serif, Arial;
+        font-family: Tahoma;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
-      }
-      
-      code {
-        font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
-          monospace;
       }
       
 `
