0b22c80421f89508b67c2b52611201075fa39e14
Emanuel Cortese <emanuelcortese_95@hotmail.com>
2022-09-29 22:19:37 -0300

    feat:  add icons, create switch, use Context and add styles for dark/light mode

--- src/components/PokeHeader/PokeHeader.jsx
+++ src/components/PokeHeader/PokeHeader.jsx
@@ -1,14 +1,31 @@
 import { Link } from 'react-router-dom'
-import { PokeHeaderStyle, PokeHeaderTitle } from './style'
+import { PokeHeaderMain, PokeHeaderTitle } from './style'
 import logo from '../../assets/Logo.png'
 import { PokeLogo } from '../PokeLogo/PokeLogo'
+import { BsSearch, BsSun, BsMoonStarsFill } from 'react-icons/bs'
+import { AiOutlineUser } from 'react-icons/ai'
+import Context from '../../context/ThemeContext'
+import { useContext } from 'react'
 
 export const PokeHeader = ({ show, title, margin }) => {
+  const { changeTheme } = useContext(Context)
   return (
-    <PokeHeaderStyle>
+    <PokeHeaderMain>
       <div>
         <PokeLogo width='100%' widhtcontainer='5rem' />
-        <input type='text' />
+        <div>
+          <BsSearch />
+          <input type='text' />
+        </div>
+        <div>
+          <label className='switchBackgound' htmlFor='switch'>
+            <BsMoonStarsFill />
+            <input type='checkbox' id='switch' onChange={() => changeTheme()} />
+            <span />
+            <BsSun />
+          </label>
+          <AiOutlineUser />
+        </div>
       </div>
       {show && (
         <Link to='/'>
@@ -20,6 +37,6 @@ export const PokeHeader = ({ show, title, margin }) => {
         {title}
       </PokeHeaderTitle>
 
-    </PokeHeaderStyle>
+    </PokeHeaderMain>
   )
 }
