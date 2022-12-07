c593579f804809d42018c2ddecb314d869d7ab3b
Emanuel Cortese <emanuelcortese_95@hotmail.com>
2022-12-06 21:43:12 -0300

    style and refactor: format code and remove React import

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,12 @@
-import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
-import { GlobalStyles } from './styles/GlobalStyles'
 import { ThemeProvider } from './context/ThemeContext'
+import { GlobalStyles } from './styles/GlobalStyles'
 
-createRoot(
-  document.getElementById('root')
-).render(
+createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <GlobalStyles />
     <ThemeProvider>
