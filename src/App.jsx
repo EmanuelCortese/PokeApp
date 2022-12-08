583483cefac700e8d1e5539a8cc661029789640d
Emanuel Cortese <emanuelcortese_95@hotmail.com>
2022-12-08 20:46:30 -0300

    feat and style: add SinglePokeProvider and format code

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
+import { useContext } from 'react'
+import { Route, Routes } from 'react-router-dom'
 import { PokeProvider } from './context/PokeContext'
 import Context from './context/ThemeContext'
-import { useContext } from 'react'
-import { Routes, Route } from 'react-router-dom'
 import { DETAIL, HOME } from './routes'
 
-import Home from './pages/Home'
 import Detail from './pages/Detail'
 import ErrorPage from './pages/Error404'
+import Home from './pages/Home'
 
 import './App.css'
+import { SinglePokeProvider } from './context/SinglePokeContext'
 
-function App () {
+function App() {
   const { userTheme } = useContext(Context)
   return (
     <div className='App' id={userTheme}>
       <PokeProvider>
-        <Routes>
-          <Route path={HOME} element={<Home />} />
-          <Route path={DETAIL} element={<Detail />} />
-          <Route path='*' element={<ErrorPage />} />
-        </Routes>
+        <SinglePokeProvider>
+          <Routes>
+            <Route path={HOME} element={<Home />} />
+            <Route path={DETAIL} element={<Detail />} />
+            <Route path='*' element={<ErrorPage />} />
+          </Routes>
+        </SinglePokeProvider>
       </PokeProvider>
     </div>
   )
