bbd4d4502536e2f9c74114569c2d990e2466f2b8
Emanuel Cortese <emanuelcortese_95@hotmail.com>
2022-12-08 21:05:35 -0300

    refactor: move states to SinglePokeContext and use dispatch

--- src/hooks/useSinglePokemon.js
+++ src/hooks/useSinglePokemon.js
@@ -1,21 +1,22 @@
-import { useEffect, useState } from 'react'
+import { useContext, useEffect } from 'react'
+import { SinglePokeContext } from '../context/SinglePokeContext'
 import getSinglePokemon from '../services/getSinglePokemon'
 
 export const useSinglePokemon = ({ id }) => {
-  const [data, setData] = useState([])
-  const [isLoading, setIsLoading] = useState(false)
-  const [error, setError] = useState(null)
+  const { dispatch } = useContext(SinglePokeContext)
 
   useEffect(() => {
-    setIsLoading(true)
-    getSinglePokemon({ id }).then(data => {
-      setIsLoading(false)
-      setData([data])
-    }).catch((err) => {
-      console.log(err)
-      err && setError(true)
-    })
-  }, [id, setData])
-
-  return { data, isLoading, error }
+    dispatch({ type: 'update_loading', payload: true })
+    dispatch({ type: 'catch_error', payload: false })
+    getSinglePokemon({ id })
+      .then((data) => {
+        dispatch({ type: 'update_loading', payload: false })
+        dispatch({ type: 'update_data', payload: [data] })
+        dispatch({ type: 'catch_error', payload: false })
+      })
+      .catch((err) => {
+        console.error(err)
+        err && dispatch({ type: 'catch_error', payload: true })
+      })
+  }, [id])
 }
