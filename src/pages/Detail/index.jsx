ae9888387d4b4fc3b8b163b270d55178dac13af6
Emanuel Cortese <emanuelcortese_95@hotmail.com>
2022-12-12 18:54:30 -0300

    refactor, feat and style: use SinglePokeContext, add PokeFooter and format code
    Get states from SinglePokeContext Context instead of useSinglePokemon hook, delete 'title' prop and add new props to PokeHeader component (margin, visible and get), move Spinner to a ternary operator, add PokeFooter component, and format code

--- src/pages/Detail/index.jsx
+++ src/pages/Detail/index.jsx
@@ -1,40 +1,49 @@
+import { useContext } from 'react'
 import { useParams } from 'react-router-dom'
-import { useSinglePokemon } from '../../hooks/useSinglePokemon'
 import { PokeDetail } from '../../components/PokeDetail/PokeDetail'
+import { PokeFooter } from '../../components/PokeFooter/PokeFooter'
+import PokeHeader from '../../components/PokeHeader/PokeHeader'
 import { Spinner } from '../../components/Spinner/Spinner'
-import ErrorPage from '../Error404'
-import { PokeHeader } from '../../components/PokeHeader/PokeHeader'
+import { SinglePokeContext } from '../../context/SinglePokeContext'
+import { useSinglePokemon } from '../../hooks/useSinglePokemon'
+import ErrorPage from '../../pages/Error404'
 
-export default function Detail () {
+export default function Detail() {
+  const {
+    state: { data, isLoading, error },
+  } = useContext(SinglePokeContext)
   const { id } = useParams()
-  const { data, isLoading, error } = useSinglePokemon({ id })
+  useSinglePokemon({ id })
 
   if (error) return <ErrorPage message='Sorry! But there was an error loading the Pokemon detail.' />
 
-  if (isLoading) return <Spinner />
-
   return (
     <>
-      <PokeHeader title='Detail' />
+      <PokeHeader margin visible={false} get={false} />
       <>
-        {data.map(({ id, name, height, weight, PokemonTypes, PokemonUrlImage, PokemonSpecies, PokemonStats }) => {
-          return (
-            <div key={id}>
-              <PokeDetail
-                id={id}
-                name={name}
-                height={height}
-                weight={weight}
-                types={PokemonTypes}
-                image={PokemonUrlImage}
-                specie={PokemonSpecies}
-                stats={PokemonStats}
-              />
-            </div>
-          )
-        })}
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          data.map(({ id, name, height, weight, PokemonTypes, PokemonUrlImage, PokemonSpecies, PokemonStats }) => {
+            return (
+              <div key={id}>
+                <PokeDetail
+                  id={id}
+                  name={name}
+                  height={height}
+                  weight={weight}
+                  types={PokemonTypes}
+                  image={PokemonUrlImage}
+                  specie={PokemonSpecies}
+                  stats={PokemonStats}
+                />
+              </div>
+            )
+          })
+        )}
       </>
 
+      <PokeFooter />
     </>
   )
 }
