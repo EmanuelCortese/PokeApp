10e25c61da279dab76b967812591e853385a684d
Emanuel Cortese <emanuelcortese_95@hotmail.com>
2022-12-13 15:48:32 -0300

    feat and style: add missing image from API and format code

--- src/components/PokeCard/PokeCard.jsx
+++ src/components/PokeCard/PokeCard.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
+import noImage from '../../assets/No-Image.png'
 import { idNumber } from '../../utils/idNumber'
 import { CardContainer, CardType } from './style'
 
-function PokeCard ({ id, name, type, image }) {
+function PokeCard({ id, name, type, image }) {
   return (
     <CardContainer to={`detail/${id}`}>
       <span>{idNumber({ id })}</span>
-      <img loading='lazy' src={image} alt={name} />
+      <img loading='lazy' src={image ?? noImage} alt={name} />
       <h2>{name}</h2>
       <div>
-        {
-            type?.map((type, index) => {
-              return <CardType PokeType={type} key={index}>{type}</CardType>
-            })
-        }
+        {type?.map((type, index) => {
+          return (
+            <CardType PokeType={type} key={index}>
+              {type}
+            </CardType>
+          )
+        })}
       </div>
     </CardContainer>
   )
