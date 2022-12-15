ec7d295cd39745a2958c50c61f2e594db54049a9
Emanuel Cortese <emanuelcortese_95@hotmail.com>
2022-12-14 22:22:16 -0300

    feat and style: add new components, condition and format code
    Add PokeSearch and PokeFooter components, use Context to add the 'keyword' for a condition to show the PokeSearch or PokeList component, change PokeHeader named import to a default import, format code

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,18 +1,29 @@
 import { usePokemons } from '../../hooks/usePokemons'
 
-import { Spinner } from '../../components/Spinner/Spinner'
+import { useContext } from 'react'
+import PokeHeader from '../../components/PokeHeader/PokeHeader'
 import PokeList from '../../components/PokeList/PokeList'
+import PokeSearch from '../../components/PokeSearch/PokeSearch'
+import { Spinner } from '../../components/Spinner/Spinner'
+import { PokeContext } from '../../context/PokeContext'
 import ErrorPage from '../Error404'
-import { PokeHeader } from '../../components/PokeHeader/PokeHeader'
 
-export default function Home () {
+import { PokeFooter } from '../../components/PokeFooter/PokeFooter'
+
+export default function Home() {
   const { loading, error } = usePokemons({ once: false })
+  const {
+    state: { keyword },
+  } = useContext(PokeContext)
   if (error) return <ErrorPage message='Sorry! But an error occurred on the server when loading the Pokemon list.' />
   if (loading) return <Spinner />
   return (
     <>
       <PokeHeader show title='Pokedex' margin />
-      <PokeList />
+
+      {keyword && keyword.length > 0 ? <PokeSearch /> : <PokeList />}
+
+      <PokeFooter />
     </>
   )
 }
