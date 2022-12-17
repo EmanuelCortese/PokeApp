275be112f0477baaf3153825e5fc1f7a6f4312d8
Emanuel Cortese <emanuelcortese_95@hotmail.com>
2022-12-16 23:23:43 -0300

    style: format code

--- src/utils/idNumber.js
+++ src/utils/idNumber.js
@@ -1,6 +1,3 @@
-
 export const idNumber = ({ id }) => {
-  return id.toString().length === 1
-    ? `#00${id}`
-    : id.toString().length === 2 ? `#0${id}` : `#${id}`
+  return id.toString().length === 1 ? `#00${id}` : id.toString().length === 2 ? `#0${id}` : `#${id}`
 }
