eb708235b460c775ec016c46378ace6151d18f75
Emanuel Cortese <emanuelcortese_95@hotmail.com>
2022-12-16 23:14:31 -0300

    feat and refactor: add and modify styles
    Change width and add padding to PokemonList, and add style to span and loader container

--- src/components/PokeList/style.js
+++ src/components/PokeList/style.js
@@ -1,10 +1,27 @@
 import styled from 'styled-components'
 
 export const PokemonList = styled.div`
-    min-height: 100vh;
-    display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(17rem, 1fr));
-    gap: 7rem;
-    width: 85%;
-    margin: 0 auto;
+  min-height: 100vh;
+  max-width: 90vw;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(17rem, 1fr));
+  gap: 7rem;
+  padding: 0 0 4rem 0;
+  margin: 0 auto;
+  > span {
+    width: 90vw;
+    color: var(--color-primary);
+    font-size: 2rem;
+    font-family: monospace;
+    font-weight: bold;
+  }
+  .loader {
+    width: 90vw;
+    height: 20vh;
+    margin: 0;
+    left: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 `
